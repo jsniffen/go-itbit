960a72ea7877cf4e62921fd61b6c638694aeb9d2
juliansniffen <jsniffen@asymmetrik.com>
2019-02-17 01:17:59 -0500

    use examples for tests

--- itbit/wallet.go
+++ itbit/wallet.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
@@ -28,44 +27,19 @@ func (c *Client) GetAllWallets(userID string, page, perPage int) ([]Wallet, erro
 		return wallets, fmt.Errorf("userID is required, got empty string")
 	}
 
-	URL := fmt.Sprintf("%s/wallets?userId=%s", Endpoint, userID)
+	url := fmt.Sprintf("%s/wallets?userId=%s", Endpoint, userID)
 
 	if page != 0 {
-		URL = fmt.Sprintf("%s?page=%d", URL, page)
+		url = fmt.Sprintf("%s?page=%d", url, page)
 	}
 
 	if perPage != 0 {
-		URL = fmt.Sprintf("%s?perPage=%d", URL, perPage)
+		url = fmt.Sprintf("%s?perPage=%d", url, perPage)
 	}
 
-	req, err := http.NewRequest(http.MethodGet, URL, nil)
+	err := c.doAuthenticatedRequest(http.MethodGet, url, nil, &wallets)
 	if err != nil {
-		return wallets, fmt.Errorf("error creating request: %v", err)
-	}
-
-	err = c.signRequest(req)
-	if err != nil {
-		return wallets, fmt.Errorf("error signing request: %v", err)
-	}
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return wallets, fmt.Errorf("error completeing request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return wallets, fmt.Errorf("error reading response: %v", err)
-	}
-
-	if resp.StatusCode != 200 {
-		return wallets, fmt.Errorf("error status not 200: %s", string(b))
-	}
-
-	err = json.Unmarshal(b, &wallets)
-	if err != nil {
-		return wallets, fmt.Errorf("error Unmarshalling response into object: %v", err)
+		return wallets, fmt.Errorf("could not do authenticated request: %v", err)
 	}
 
 	return wallets, nil
@@ -92,37 +66,9 @@ func (c *Client) CreateNewWallet(userID, walletName string) (Wallet, error) {
 		return wallet, fmt.Errorf("could not marshal bodyMap: %v", err)
 	}
 
-	URL := Endpoint + "/wallets"
-	req, err := http.NewRequest(http.MethodPost, URL, bytes.NewBuffer(bodyJSON))
-	if err != nil {
-		return wallet, fmt.Errorf("error creating request: %v", err)
-	}
+	url := fmt.Sprintf("%s/%s", Endpoint, wallet)
 
-	err = c.signRequest(req)
-	if err != nil {
-		return wallet, fmt.Errorf("error signing request: %v", err)
-	}
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return wallet, fmt.Errorf("could not do request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return wallet, fmt.Errorf("error reading response: %v", err)
-	}
-
-	if resp.StatusCode != 200 {
-		return wallet, fmt.Errorf("error status not 200: %s", string(b))
-	}
-
-	fmt.Printf("%v", b)
-	err = json.Unmarshal(b, &wallet)
-	if err != nil {
-		return wallet, fmt.Errorf("could not unmarshal response body into wallet: %v", err)
-	}
+	c.doAuthenticatedRequest(http.MethodPost, url, bytes.NewBuffer(bodyJSON), &wallet)
 
 	return wallet, nil
 }
@@ -134,36 +80,11 @@ func (c *Client) GetWallet(walletID string) (Wallet, error) {
 		return wallet, fmt.Errorf("walletID required, got empty string")
 	}
 
-	URL := fmt.Sprintf("%s/wallets/%s", Endpoint, walletID)
+	url := fmt.Sprintf("%s/wallets/%s", Endpoint, walletID)
 
-	req, err := http.NewRequest(http.MethodGet, URL, nil)
+	err := c.doAuthenticatedRequest(http.MethodGet, url, nil, &wallet)
 	if err != nil {
-		return wallet, fmt.Errorf("could not create request: %v", err)
-	}
-
-	err = c.signRequest(req)
-	if err != nil {
-		return wallet, fmt.Errorf("could not sign request: %v", err)
-	}
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return wallet, fmt.Errorf("could not do request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return wallet, fmt.Errorf("could not read response body: %v", err)
-	}
-
-	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return wallet, fmt.Errorf("%s: %s", resp.Status, string(b))
-	}
-
-	err = json.Unmarshal(b, &wallet)
-	if err != nil {
-		return wallet, fmt.Errorf("error Unmarshalling response body into wallet: %v", err)
+		fmt.Errorf("could not do authenticated request: %v", err)
 	}
 
 	return wallet, nil
--- itbit/wallet_test.go
+++ itbit/wallet_test.go
@@ -1,7 +1,9 @@
 package itbit_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -12,19 +14,155 @@ import (
 	"github.com/juliansniff/go-itbit/itbit"
 )
 
-func TestMain(m *testing.M) {
-	r := mux.NewRouter()
-	r.HandleFunc("/wallets/{id}/balances/{currency}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		currency := vars["currency"]
-
-		response := fmt.Sprintf(`{
+func handleGetWalletBalance(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	currency := vars["currency"]
+	response := fmt.Sprintf(`{
 				"currency": "%s",
 				"availableBalance": "0",
 				"totalBalance": "0"
 			}`, currency)
-		fmt.Fprintf(w, response)
-	})
+	fmt.Fprintf(w, response)
+}
+
+func handleGetWallet(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	response := fmt.Sprintf(`{
+			"id": "%s",
+			"userId": "userID",
+			"name": "Test Wallet",
+			"balances": [
+				{
+					"currency": "USD",
+					"availableBalance": "50000.0000000",
+					"totalBalance": "50000.0000000"
+				},
+				{
+					"currency": "XBT",
+					"availableBalance": "100.00000000",
+					"totalBalance": "100.00000000"
+				},
+				{
+					"currency": "EUR",
+					"availableBalance": "100000.00000000",
+					"totalBalance": "100000.00000000"
+				},
+				{
+					"currency": "SGD",
+					"availableBalance": "515440.88288502",
+					"totalBalance": "515432.74228603"
+				}
+			]
+		}`, id)
+	fmt.Fprintf(w, response)
+}
+
+func handleGetAllWallets(w http.ResponseWriter, r *http.Request) {
+	response := `[
+			{
+				"id": "f46eb6b0-2a7f-4c07-898e-856d58568fde",
+				"userId": "e9c856db-c726-46b2-ace0-a806671105fb",
+				"name": "Wallet 1",
+				"balances": [
+					{
+						"currency": "USD",
+						"availableBalance": "0.00000000",
+						"totalBalance": "0.00000000"
+					},
+					{
+						"currency": "XBT",
+						"availableBalance": "0.00000000",
+						"totalBalance": "0.00000000"
+					},
+					{
+						"currency": "EUR",
+						"availableBalance": "0.00000000",
+						"totalBalance": "0.00000000"
+					},
+					{
+						"currency": "SGD",
+						"availableBalance": "0.00000000",
+						"totalBalance": "0.00000000"
+					}
+				]
+			},
+			{
+				"id": "2224db00-da07-46f7-ba37-b877e1cd455a",
+				"userId": "e9c856db-c726-46b2-ace0-a806671105fb",
+				"name": "Wallet 2",
+				"balances": [
+					{
+						"currency": "USD",
+						"availableBalance": "75631.88785992",
+						"totalBalance": "75631.88785999"
+					},
+					{
+						"currency": "XBT",
+						"availableBalance": "100100.03000000",
+						"totalBalance": "100100.03000000"
+					},
+					{
+						"currency": "EUR",
+						"availableBalance": "100000.00000000",
+						"totalBalance": "100000.00000000"
+					},
+					{
+						"currency": "SGD",
+						"availableBalance": "100000.00000000",
+						"totalBalance": "100000.00000000"
+					}
+				]
+			}
+		]`
+	fmt.Fprintf(w, response)
+}
+
+func handleCreateNewWallet(w http.ResponseWriter, r *http.Request) {
+	b, _ := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	var body map[string]string
+	_ = json.Unmarshal(b, &body)
+
+	name := body["name"]
+	userID := body["userId"]
+
+	response := fmt.Sprintf(`{
+				"id": "walletID",
+				"userId": "%s",
+				"name": "%s",
+				"balances": [
+					{
+						"currency": "USD",
+						"availableBalance": "0.00000000",
+						"totalBalance": "0.00000000"
+					},
+					{
+						"currency": "XBT",
+						"availableBalance": "0.00000000",
+						"totalBalance": "0.00000000"
+					},
+					{
+						"currency": "EUR",
+						"availableBalance": "0.00000000",
+						"totalBalance": "0.00000000"
+					},
+					{
+						"currency": "SGD",
+						"availableBalance": "0.00000000",
+						"totalBalance": "0.00000000"
+					}
+				]
+			}`, userID, name)
+	fmt.Fprintf(w, response)
+}
+
+func TestMain(m *testing.M) {
+	r := mux.NewRouter()
+	r.HandleFunc("/wallets/{id}/balances/{currency}", handleGetWalletBalance).Methods(http.MethodGet)
+	r.HandleFunc("/wallets/{id}", handleGetWallet).Methods(http.MethodGet)
+	r.HandleFunc("/wallets", handleGetAllWallets).Methods(http.MethodGet)
+	r.HandleFunc("/wallets", handleCreateNewWallet).Methods(http.MethodPost)
 	ts := httptest.NewServer(r)
 	defer ts.Close()
 
@@ -32,298 +170,64 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-// func TestGetAllWallets(t *testing.T) {
-// 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		response := `[
-// 			{
-// 				"id": "f46eb6b0-2a7f-4c07-898e-856d58568fde",
-// 				"userId": "e9c856db-c726-46b2-ace0-a806671105fb",
-// 				"name": "user-wallet-01",
-// 				"balances": [
-// 					{
-// 						"currency": "USD",
-// 						"availableBalance": "0.00000000",
-// 						"totalBalance": "0.00000000"
-// 					},
-// 					{
-// 						"currency": "XBT",
-// 						"availableBalance": "0.00000000",
-// 						"totalBalance": "0.00000000"
-// 					},
-// 					{
-// 						"currency": "EUR",
-// 						"availableBalance": "0.00000000",
-// 						"totalBalance": "0.00000000"
-// 					},
-// 					{
-// 						"currency": "SGD",
-// 						"availableBalance": "0.00000000",
-// 						"totalBalance": "0.00000000"
-// 					}
-// 				]
-// 			},
-// 			{
-// 				"id": "2224db00-da07-46f7-ba37-b877e1cd455a",
-// 				"userId": "e9c856db-c726-46b2-ace0-a806671105fb",
-// 				"name": "user-wallet-02",
-// 				"balances": [
-// 					{
-// 						"currency": "USD",
-// 						"availableBalance": "75631.88785992",
-// 						"totalBalance": "75631.88785999"
-// 					},
-// 					{
-// 						"currency": "XBT",
-// 						"availableBalance": "100100.03000000",
-// 						"totalBalance": "100100.03000000"
-// 					},
-// 					{
-// 						"currency": "EUR",
-// 						"availableBalance": "100000.00000000",
-// 						"totalBalance": "100000.00000000"
-// 					},
-// 					{
-// 						"currency": "SGD",
-// 						"availableBalance": "100000.00000000",
-// 						"totalBalance": "100000.00000000"
-// 					}
-// 				]
-// 			}
-// 		]`
-// 		fmt.Fprintf(w, response)
-// 	}))
-// 	defer ts.Close()
-//
-// 	endpoint = ts.URL
-// 	c := NewClient("key", "secret")
-//
-// 	got, err := c.GetAllWallets("userID", 0, 0)
-// 	if err != nil {
-// 		t.Errorf("error getting all wallets: %v", err)
-// 		return
-// 	}
-//
-// 	expected := []Wallet{
-// 		Wallet{
-// 			ID:     "f46eb6b0-2a7f-4c07-898e-856d58568fde",
-// 			UserID: "e9c856db-c726-46b2-ace0-a806671105fb",
-// 			Name:   "user-wallet-01",
-// 			Balances: []Balance{
-// 				{
-// 					Currency:         "USD",
-// 					AvailableBalance: 0.00000000,
-// 					TotalBalance:     0.00000000,
-// 				},
-// 				{
-// 					Currency:         "XBT",
-// 					AvailableBalance: 0.00000000,
-// 					TotalBalance:     0.00000000,
-// 				},
-// 				{
-// 					Currency:         "EUR",
-// 					AvailableBalance: 0.00000000,
-// 					TotalBalance:     0.00000000,
-// 				},
-// 				{
-// 					Currency:         "SGD",
-// 					AvailableBalance: 0.00000000,
-// 					TotalBalance:     0.00000000,
-// 				},
-// 			},
-// 		},
-// 		Wallet{
-// 			ID:     "2224db00-da07-46f7-ba37-b877e1cd455a",
-// 			UserID: "e9c856db-c726-46b2-ace0-a806671105fb",
-// 			Name:   "user-wallet-02",
-// 			Balances: []Balance{
-// 				{
-// 					Currency:         "USD",
-// 					AvailableBalance: 75631.88785992,
-// 					TotalBalance:     75631.88785999,
-// 				},
-// 				{
-// 					Currency:         "XBT",
-// 					AvailableBalance: 100100.03000000,
-// 					TotalBalance:     100100.03000000,
-// 				},
-// 				{
-// 					Currency:         "EUR",
-// 					AvailableBalance: 100000.00000000,
-// 					TotalBalance:     100000.00000000,
-// 				},
-// 				{
-// 					Currency:         "SGD",
-// 					AvailableBalance: 100000.00000000,
-// 					TotalBalance:     100000.00000000,
-// 				},
-// 			},
-// 		},
-// 	}
-//
-// 	if !reflect.DeepEqual(got, expected) {
-// 		t.Errorf("got: %v, expected: %v", got, expected)
-// 	}
-// }
-//
-// func TestCreateNewWallet(t *testing.T) {
-// 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		b, _ := ioutil.ReadAll(r.Body)
-// 		r.Body.Close()
-// 		var body map[string]string
-// 		_ = json.Unmarshal(b, &body)
-//
-// 		name := body["name"]
-// 		userID := body["userId"]
-//
-// 		response := fmt.Sprintf(`{
-// 				"id": "walletID",
-// 				"userId": "%s",
-// 				"name": "%s",
-// 				"balances": [
-// 					{
-// 						"currency": "USD",
-// 						"availableBalance": "0.00000000",
-// 						"totalBalance": "0.00000000"
-// 					},
-// 					{
-// 						"currency": "XBT",
-// 						"availableBalance": "0.00000000",
-// 						"totalBalance": "0.00000000"
-// 					},
-// 					{
-// 						"currency": "EUR",
-// 						"availableBalance": "0.00000000",
-// 						"totalBalance": "0.00000000"
-// 					},
-// 					{
-// 						"currency": "SGD",
-// 						"availableBalance": "0.00000000",
-// 						"totalBalance": "0.00000000"
-// 					}
-// 				]
-// 			}`, userID, name)
-// 		fmt.Fprintf(w, response)
-// 	}))
-// 	defer ts.Close()
-//
-// 	endpoint = ts.URL
-// 	c := NewClient("key", "secret")
-// 	got, err := c.CreateNewWallet("userID", "walletName")
-// 	if err != nil {
-// 		t.Errorf("%v", err)
-// 		return
-// 	}
-//
-// 	expected := Wallet{
-// 		ID:     "walletID",
-// 		UserID: "userID",
-// 		Name:   "walletName",
-// 		Balances: []Balance{
-// 			{
-// 				Currency:         "USD",
-// 				AvailableBalance: 0.00000000,
-// 				TotalBalance:     0.00000000,
-// 			},
-// 			{
-// 				Currency:         "XBT",
-// 				AvailableBalance: 0.00000000,
-// 				TotalBalance:     0.00000000,
-// 			},
-// 			{
-// 				Currency:         "EUR",
-// 				AvailableBalance: 0.00000000,
-// 				TotalBalance:     0.00000000,
-// 			},
-// 			{
-// 				Currency:         "SGD",
-// 				AvailableBalance: 0.00000000,
-// 				TotalBalance:     0.00000000,
-// 			},
-// 		},
-// 	}
-//
-// 	if !reflect.DeepEqual(got, expected) {
-// 		t.Errorf("got: %v, expected: %v", got, expected)
-// 	}
-// }
-//
-// func TestGetWallet(t *testing.T) {
-// 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		path := r.URL.Path
-// 		pathArray := strings.Split(path, "/")
-// 		walletID := pathArray[len(pathArray)-1]
-// 		response := fmt.Sprintf(`{
-// 			"id": "%s",
-// 			"userId": "userID",
-// 			"name": "walletName",
-// 			"balances": [
-// 				{
-// 					"currency": "USD",
-// 					"availableBalance": "50000.0000000",
-// 					"totalBalance": "50000.0000000"
-// 				},
-// 				{
-// 					"currency": "XBT",
-// 					"availableBalance": "100.00000000",
-// 					"totalBalance": "100.00000000"
-// 				},
-// 				{
-// 					"currency": "EUR",
-// 					"availableBalance": "100000.00000000",
-// 					"totalBalance": "100000.00000000"
-// 				},
-// 				{
-// 					"currency": "SGD",
-// 					"availableBalance": "515440.88288502",
-// 					"totalBalance": "515432.74228603"
-// 				}
-// 			]
-// 		}`, walletID)
-// 		fmt.Fprintf(w, response)
-// 	}))
-// 	defer ts.Close()
-//
-// 	endpoint = ts.URL
-// 	c := NewClient("key", "secret")
-//
-// 	got, err := c.GetWallet("walletID")
-// 	if err != nil {
-// 		t.Errorf("error getting wallets: %v", err)
-// 		return
-// 	}
-//
-// 	expected := Wallet{
-// 		ID:     "walletID",
-// 		UserID: "userID",
-// 		Name:   "walletName",
-// 		Balances: []Balance{
-// 			{
-// 				Currency:         "USD",
-// 				AvailableBalance: 50000.0000000,
-// 				TotalBalance:     50000.0000000,
-// 			},
-// 			{
-// 				Currency:         "XBT",
-// 				AvailableBalance: 100.00000000,
-// 				TotalBalance:     100.00000000,
-// 			},
-// 			{
-// 				Currency:         "EUR",
-// 				AvailableBalance: 100000.00000000,
-// 				TotalBalance:     100000.00000000,
-// 			},
-// 			{
-// 				Currency:         "SGD",
-// 				AvailableBalance: 515440.88288502,
-// 				TotalBalance:     515432.74228603,
-// 			},
-// 		},
-// 	}
-//
-// 	if !reflect.DeepEqual(got, expected) {
-// 		t.Errorf("got: %v, expected: %v", got, expected)
-// 	}
-// }
+func ExampleClient_GetAllWallets() {
+	c := itbit.NewClient("key", "secret")
+
+	wallets, err := c.GetAllWallets("userID", 0, 0)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for _, wallet := range wallets {
+		fmt.Printf("%s:\n", wallet.Name)
+		for _, balance := range wallet.Balances {
+			fmt.Printf("%10.2f %s\t", balance.AvailableBalance, balance.Currency)
+		}
+		fmt.Printf("\n")
+	}
+	// Wallet 1:
+	// 0.00 USD	      0.00 XBT	      0.00 EUR	      0.00 SGD
+	// Wallet 2:
+	// 75631.89 USD	 100100.03 XBT	 100000.00 EUR	 100000.00 SGD
+}
+
+func ExampleClient_CreateNewWallet() {
+	c := itbit.NewClient("key", "secret")
+	wallet, err := c.CreateNewWallet("userID", "New Wallet")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Printf("%s:\n", wallet.Name)
+	for _, balance := range wallet.Balances {
+		fmt.Printf("%.2f %s Available\n", balance.AvailableBalance, balance.Currency)
+	}
+	// New Wallet:
+	// 0.00 USD Available
+	// 0.00 XBT Available
+	// 0.00 EUR Available
+	// 0.00 SGD Available
+}
+
+func ExampleClient_GetWallet() {
+	c := itbit.NewClient("key", "secret")
+
+	wallet, err := c.GetWallet("testWalletID")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Printf("%s:\n", wallet.Name)
+	for _, balance := range wallet.Balances {
+		fmt.Printf("%.2f %s Available\n", balance.AvailableBalance, balance.Currency)
+	}
+	// Output:
+	// Test Wallet:
+	// 50000.00 USD Available
+	// 100.00 XBT Available
+	// 100000.00 EUR Available
+	// 515440.88 SGD Available
+}
 
 func ExampleClient_GetWalletBalance() {
 	c := itbit.NewClient("key", "secret")
